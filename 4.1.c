77e25f96478b4b72a3faba2d829f34e8274e0592
nunaazkrn <2020862276@student.uitm.edu.my>
2022-06-23 18:17:17 +0000

    compiling 4.1.c - Receiving Data from Remote Server

--- 4.1.c
+++ 4.1.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<arpa/inet.h>	//inet_addr
-#include <string.h>
+#include<string.h>
+#include <unistd.h>
 
 int main(int argc , char *argv[])
 {
 	int socket_desc;
-	struct sockaddr_in server; 
-        char *message;
+	struct sockaddr_in server;
+	char *message , server_reply[2000];
 	
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -37,7 +38,18 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 	puts("Data Send\n");
-        return 0;
+                    //Receive a reply from the server
+	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+	{
+		puts("recv failed");
+	}
+	puts("Reply received\n");
+	puts(server_reply);
+	close(socket_desc);
+	return 0;
 }
+
+
+
 
 
