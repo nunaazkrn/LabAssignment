7271b55cca9b77b251e14e9873e3e1a599d09e9d
nunaazkrn <2020862276@student.uitm.edu.my>
2022-06-25 10:45:55 +0000

    compile 5.4

---
+++ 5.4.client.py
@@ -0,0 +1,15 @@
+from netmiko import ConnectHandler
+
+myserver = {
+    'device_type': 'linux',
+    'host':   '192.168.53.3',  #Your Server IP
+    'username': 'nuna', #your Server Username
+    'password': 'nuna9679', #your server password
+    'port' : 22,
+    'secret': '',
+}
+
+net_connect = ConnectHandler(**myserver)
+output = net_connect.send_command('uname -a')
+print(output)
+
